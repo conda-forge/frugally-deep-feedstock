c60502473202b56a7cfa58609e9b5a03856035e2
Utkarsh Raj <encode.sh@protonmail.com>
2026-06-30 22:28:30 +0530

    Set CMAKE_POLICY_VERSION_MINIMUM=3.5

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ mkdir build && cd build
 cmake ${CMAKE_ARGS} \
       -DCMAKE_BUILD_TYPE=Release \
       -DFDEEP_USE_DOUBLE=ON \
+      -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
       $SRC_DIR
 
 make -j${CPU_COUNT}
